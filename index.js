3c1205740cdb40ff608e34f19a0df02400c3007d
fadimoayad <fadimoayad99@yahoo.com>
2022-10-14 21:28:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+const dodger=document.getElementById('dodger');
+
+dodger.style.backgroundColor="#FF69B4";
+
+dodger.style.bottom="0px";
+
+
+document.addEventListener('keydown', function(e){
+    if(e.key==="ArrowLeft"){
+        moveDodgerLeft();
+    }else if(e.key==="ArrowRight"){
+        moveDodgerRight();
+    }
+
+})
+
+function moveDodgerLeft(){
+    const leftNumber=dodger.style.left.replace("px","");
+    const left=parseInt(leftNumber,10);
+
+    if(left>0){
+
+    dodger.style.left=`${left-1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    const rightNumber=dodger.style.left.replace("px","");
+    const right=parseInt(rightNumber,10);
+    if(right<360){
+    dodger.style.left=`${right+1}px`;
+    }
+}
